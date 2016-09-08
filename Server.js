54768be64690b8cf5ac6bfa16f682b14c2414f8f
赵夕 <aska_wasaby@hotmail.com>
2016-09-08 10:55:36 +0800

    初次提交

---
+++ Server.js
@@ -0,0 +1,28 @@
+/// <reference path=".\NodeSnippet\typings\index.d.ts" />
+
+var express = require('express');
+var app = express();
+var http = require('http');
+
+var ser = http.createServer(app);
+var io = require('socket.io')(ser);
+
+io.on('connection', function (client) {
+    console.log('connection');
+    client.on('Send', function (data) {
+        io.emit('Send', { aa: 'aa' });
+    });
+    client.on('event', function (data) {
+        console.log(data);
+    });
+    client.on('disconnect', function () {
+        console.log('disconnect');
+    });
+});
+ser.listen(3000);
+
+
+
+
+
+
